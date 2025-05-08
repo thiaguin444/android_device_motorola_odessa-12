c583495142396b27c159a31ec74e47eca1ff6c4e
thiago⁴⁴ <thiagojurema@protonmail.com>
2025-05-08 10:14:50 -0300

    bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,12 @@ deviceDir=$(gettop)/device/motorola/odessa
 # permission
 chmod +x device/motorola/odessa/applyPatches.sh
 
+rm -rf external/zlib-ng
+rm -rf external-zlib
+rm -rf external/jemalloc_new
+
+git clone https://github.com/ProtonAOSP/android_external_zlib-ng.git -b sc-qpr3 externel/zlib-ng
+git clone https://github.com/ProtonAOSP/android_external_jemalloc_new.git -b sc-qpr3 external/jemalloc_new
+
 # apply patches
 ${deviceDir}/applyPatches.sh ${deviceDir}/patches
