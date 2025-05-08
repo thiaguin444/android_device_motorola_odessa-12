465362cb1aa33fd55a5dea6c88f4e0bbd4e4420d
thiaguin444 <thiagojurema@protonmail.com>
2025-05-08 11:33:03 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ deviceDir=$(gettop)/device/motorola/odessa
 chmod +x device/motorola/odessa/applyPatches.sh
 
 rm -rf external/zlib-ng
-rm -rf external-zlib
+rm -rf external/zlib
 rm -rf external/jemalloc_new
 
 git clone https://github.com/ProtonAOSP/android_external_zlib-ng.git -b sc-qpr3 externel/zlib-ng
